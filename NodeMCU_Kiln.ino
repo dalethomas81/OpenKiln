3b972f8572b22a77bcd82631a5f062342d78a304
Dale <dalethomas81@gmail.com>
2020-07-28 01:15:41 -0400

    check

--- NodeMCU_Kiln.ino
+++ NodeMCU_Kiln.ino
@@ -32,14 +32,9 @@ bool Safety_Ok = false;
 uint16_t Mode = AUTOMATIC_MODE,  Mode_Last = AUTOMATIC_MODE;
 bool ThermalRunawayDetected = false, ui_ThermalRunawayOverride = false;
 
-
-
-
-
-
-
-
-
+//
+// web server 
+//
 #define WIFI_LISTENING_PORT       80
 #include <WebSocketsServer.h>  // Websockets by Markus Sattler https://github.com/Links2004/arduinoWebSockets
 WebSocketsServer webSocket = WebSocketsServer(WIFI_LISTENING_PORT+1);
@@ -47,18 +42,10 @@ WebSocketsServer webSocket = WebSocketsServer(WIFI_LISTENING_PORT+1);
 #include <ESPAsyncWebServer.h> // https://github.com/me-no-dev/ESPAsyncWebServer
 AsyncWebServer server(WIFI_LISTENING_PORT);
 
-
-
-
-
-
-
-
-
 //
 // wifi
 //
-#include <ESP8266WiFi.h>
+//#include <ESP8266WiFi.h>
 #define WIFI_SSID                 "Thomas_301"
 #define WIFI_PASSWORD             "RS232@12"
 void connectWifi(int delaytime) { 
@@ -84,6 +71,42 @@ void checkWifi() {
     WiFi.reconnect();
     //connectWifi(0);
   }
+}
+
+//
+// ota 
+//
+#include <ArduinoOTA.h>
+#define OTA_PASSWORD "ProFiBus@12"
+#define OTA_HOSTNAME "Kiln-"
+void setupOTA(){
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+  // Hostname defaults to esp8266-[ChipID]
+  String hostname(OTA_HOSTNAME);
+  hostname += String(ESP.getChipId(), HEX);
+  ArduinoOTA.setHostname(hostname.c_str());
+  // No authentication by default
+  //ArduinoOTA.setPassword((const char *)OTA_PASSWORD);
+  ArduinoOTA.onStart([]() {
+    LittleFS.end();
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
 }
 
 //
@@ -149,42 +172,6 @@ void initLittleFS() {
         }
     }
     */
-}
-
-//
-// ota 
-//
-#include <ArduinoOTA.h>
-#define OTA_PASSWORD "ProFiBus@12"
-#define OTA_HOSTNAME "Kiln-"
-void setupOTA(){
-  // Port defaults to 8266
-  // ArduinoOTA.setPort(8266);
-  // Hostname defaults to esp8266-[ChipID]
-  String hostname(OTA_HOSTNAME);
-  hostname += String(ESP.getChipId(), HEX);
-  ArduinoOTA.setHostname(hostname.c_str());
-  // No authentication by default
-  //ArduinoOTA.setPassword((const char *)OTA_PASSWORD);
-  ArduinoOTA.onStart([]() {
-    LittleFS.end();
-    Serial.println("Start");
-  });
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
-  });
-  ArduinoOTA.begin();
 }
 
 //
