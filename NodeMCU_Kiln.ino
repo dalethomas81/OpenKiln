202ddb16281f8e3598f5881ddbadd06026ffe028
Dale <dalethomas81@gmail.com>
2020-07-28 01:17:08 -0400

    check

--- NodeMCU_Kiln.ino
+++ NodeMCU_Kiln.ino
@@ -74,42 +74,6 @@ void checkWifi() {
 }
 
 //
-// ota 
-//
-#include <ArduinoOTA.h>
-#define OTA_PASSWORD "ProFiBus@12"
-#define OTA_HOSTNAME "Kiln-"
-void setupOTA(){
-  // Port defaults to 8266
-  // ArduinoOTA.setPort(8266);
-  // Hostname defaults to esp8266-[ChipID]
-  String hostname(OTA_HOSTNAME);
-  hostname += String(ESP.getChipId(), HEX);
-  ArduinoOTA.setHostname(hostname.c_str());
-  // No authentication by default
-  //ArduinoOTA.setPassword((const char *)OTA_PASSWORD);
-  ArduinoOTA.onStart([]() {
-    LittleFS.end();
-    Serial.println("Start");
-  });
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
-  });
-  ArduinoOTA.begin();
-}
-
-//
 // flash 
 //
 #include "LittleFS.h" // need the upload tool here https://github.com/earlephilhower/arduino-esp8266littlefs-plugin
@@ -172,6 +136,42 @@ void initLittleFS() {
         }
     }
     */
+}
+
+//
+// ota 
+//
+#include <ArduinoOTA.h>
+#define OTA_PASSWORD "ProFiBus@12"
+#define OTA_HOSTNAME "Kiln-"
+void setupOTA(){
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+  // Hostname defaults to esp8266-[ChipID]
+  String hostname(OTA_HOSTNAME);
+  hostname += String(ESP.getChipId(), HEX);
+  ArduinoOTA.setHostname(hostname.c_str());
+  // No authentication by default
+  //ArduinoOTA.setPassword((const char *)OTA_PASSWORD);
+  ArduinoOTA.onStart([]() {
+    LittleFS.end();
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
 }
 
 //
